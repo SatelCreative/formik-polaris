c189d71f90401a6da46e633f944868d6c41c167e
Bob Roan <roanvanbao@gmail.com>
2020-03-17 02:20:00 +0700

    Add component RangerSlider (#104)

---
+++ src/RangeSlider.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { RangeSlider as PolarisRangeSlider } from '@shopify/polaris';
+import { RangeSliderProps as PolarisRangeSliderProps, RangeSliderValue } from '@shopify/polaris/types/components/RangeSlider';
+import { usePolarisField, UsePolarisFieldProps } from './usePolarisField';
+import { Omit } from './types';
+
+type Props<V> = UsePolarisFieldProps<V, RangeSliderValue> & PolarisRangeSliderProps;
+
+export type RangeSliderProps<V = any> = Omit<
+  Props<V>,
+  'id' | 'value' | 'onChange' | 'onBlur' | 'onFocus' | 'error'
+>;
+
+function RangeSlider<V = any>(props: RangeSliderProps<V>) {
+  const { name, encode, decode, validate, ...polarisProps } = props;
+
+  const {
+    value: rawValue,
+    isSubmitting,
+    handleFocus,
+    handleBlur,
+    handleChange,
+    error,
+  } = usePolarisField<V, RangeSliderValue>({ name, encode, decode, validate });
+
+  const value = rawValue === undefined ? '' : rawValue;
+  if (typeof value !== 'number' && !Array.isArray(value)) {
+    throw new Error(
+      `[RangeSlider] Found value of type "${typeof value}" for field "${name}". Requires number or [number, number] (after decode)`,
+    );
+  }
+
+  return (
+    <PolarisRangeSlider
+      disabled={isSubmitting}
+      {...polarisProps}
+      id={name}
+      value={value}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onChange={handleChange}
+      error={error}
+    />
+  );
+}
+
+export default RangeSlider;
